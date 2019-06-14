0209d088d6bb92617b91296b10f74598e34284e5
Santosh Tirunagari <tsantosh7@gmail.com>
2019-06-14 16:22:43 +0100

    git ignore added

--- Code/Download_EPMC_XML_Dumps.py
+++ Code/Download_EPMC_XML_Dumps.py
@@ -13,19 +13,12 @@ if __name__ == '__main__':
     max_pool_size = 4
     pool = multiprocessing.Pool(cpus if cpus < max_pool_size else max_pool_size)
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    target = "NEWCODE"
-    prefix_list = ["NWZV1WB", "AWU3JAD", "NW96MRD"]
+    target = '/home/santosh/Work/EPMC_xml_dumps/'
+    xml_dumps_url = 'https://europepmc.org/ftp/oa/'
     download_list = []
-    name_list = list(range(1, 23))
-    name_list.extend(["zoom_side", "zoom_sole", "zoom_side-thumb"])
-    for prefix in prefix_list:
-        path = os.path.join(base_dir, prefix)
-        if not os.path.exists(path):
-            os.mkdir(path)
-        if not os.path.isdir(path):
-            exit()
-        for name in name_list:
-            download_list.append(['https://img2.tennis-warehouse.com/360/{p}/{n}.jpg'.format(n=name, p=prefix), path])
+
+
+    download_list.append([xml_dumps_url.format(n=name, p=prefix), path])
 
     for url, path in download_list: # change here to download other files
         print('Beginning file download with wget module {n}'.format(n=url))
