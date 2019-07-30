d547de7019779e9a28e987c46632ad85e91601ee
Santosh <tsantosh7@gmail.com>
2019-07-30 17:29:25 +0100

    New dataset

--- Code/Count_Words_from_PMC_Articles.py
+++ Code/Count_Words_from_PMC_Articles.py
@@ -17,20 +17,20 @@ all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 #combine all files in the list
 combined_csv = pd.concat([pd.read_csv(f, sep='\t', error_bad_lines=False) for f in all_filenames ])
 
+
 appended_data = []
 for f in all_filenames:
     print(f)
-    temp_ = pd.read_csv(all_filenames[0], sep='\t', error_bad_lines=False, names = ['words', 'year', 'freq', 'papers'], header =None)
-    temp_['TF'] = temp_['freq'] * temp_['papers']
-    temp = temp_[['words','TF']]
-
+    temp_ = pd.read_csv(f, sep='\t', error_bad_lines=False, names = ['words', 'year', 'freq', 'papers'], header =None)
+    # temp_['TF'] = temp_['freq'] * temp_['papers']
+    temp = temp_[['words','freq']]
     appended_data.append(temp)
-    appended_data = pd.concat(appended_data)
-    appended_data = appended_data.groupby(['words'], as_index=False).agg('sum')
 
 
 
+appended_data = pd.concat(appended_data)
+# appended_data = appended_data.groupby(['words'], as_index=False).agg('sum')
 #export to csv
-combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
+appended_data.to_csv(data_path+"1-grams_duplicates.csv", chunksize=1000, index=False, encoding='utf-8-sig')
 
 
