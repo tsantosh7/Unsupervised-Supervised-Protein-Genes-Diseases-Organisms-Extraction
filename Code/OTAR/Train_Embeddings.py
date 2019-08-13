ba55be76443f516816d838885dbbfd0dfd84fafa
Santosh Tirunagari <s.tirunagari@synoptica.com>
2019-08-13 22:31:39 +0100

    word2vec on OTAR

---
+++ Code/OTAR/Train_Embeddings.py
@@ -0,0 +1,58 @@
+# Here I am using gensim for learning the word embeddings from the EUADR corpus
+# This is just a rough start to learn
+import gensim
+import logging
+import os
+import pandas as pd
+import re
+# start the log
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',level=logging.INFO)
+
+
+# download NLTK basic stopwords
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+
+
+def clean_my_text(full_text):
+    stopset = set(stopwords.words('english'))  # | set(string.punctuation)
+    tokens = nltk.word_tokenize(full_text)
+    cleanup = [token for token in tokens if token not in stopset and len(token) > 2]
+    return cleanup
+
+
+# get the current working directory and file
+data_dir_path = '/home/stirunag/Dataset/'
+
+
+# tokenize.sent_tokenize(doc.lower())
+class SentenceClass(object):
+    def __init__(self, dirname):
+        self.dirname = dirname
+
+    def __iter__(self):
+        for fname in os.listdir(self.dirname):
+            print(fname)
+            for line in open(os.path.join(self.dirname, fname), 'r', encoding='ISO-8859-1'):
+                yield clean_my_text(line)
+
+
+
+
+# iterate one line at a time and learn embeddings
+sentences = SentenceClass(data_dir_path)  # a memory-friendly iterator
+# model = gensim.models.Word2Vec(sentences)
+
+model = gensim.models.Word2Vec(
+    sentences,
+    size=200,
+    window=5,
+    min_count=2,
+    workers=8, iter=10)
+
+logging.info("Done training data files")
+
+
+model.wv.save_word2vec_format('/home/stirunag/Dataset/model_OTAR.bin', binary=True)
