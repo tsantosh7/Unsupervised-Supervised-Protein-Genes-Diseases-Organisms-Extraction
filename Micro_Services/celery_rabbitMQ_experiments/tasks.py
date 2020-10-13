5e39b5c64da2b8008fd72c3a4ec979246b55c185
Santosh Tirunagari <tsantosh7@gmail.com>
2020-10-13 18:12:03 +0100

    extract GP DS from OTAR

--- Micro_Services/celery_rabbitMQ_experiments/tasks.py
+++ Micro_Services/celery_rabbitMQ_experiments/tasks.py
@@ -1,6 +1,10 @@
 #tasks.py
 from celery import Celery
 app = Celery('tasks', backend='rpc://', broker='pyamqp://capo:capo@ai-capo-api-lb-2.ebi.ac.uk:5672/capo_rabbit_host')
+
 @app.task
 def sentence_length(sentence):
     return len(sentence)
+
+#def sentence_name(sentence):
+#    return sentence
